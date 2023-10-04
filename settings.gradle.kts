bc5fc9e7f52345c0be81e1382082d295094d8774
eduardwilliam <willitairas@gmail.com>
2023-10-04 15:55:44 +0700

    push

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Determine Most Improve Basketball Player"
+include(":app")
+ 
